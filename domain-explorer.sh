cd06329bac78812b3ab1267f201c1061663358d3
Anastasiia Piontkovska <149788908+AnastasiiaPion@users.noreply.github.com>
2023-12-19 12:21:29 +0100

    Update domain-explorer.sh
    Reduce retryings for wget

--- domain-explorer.sh
+++ domain-explorer.sh
@@ -29,7 +29,7 @@ function main(){
         echo "==> Downloading index file..."                    # Starting downloading the index.html file
     fi
 
-    wget "https://$URL" --no-check-certificate --tries=5
+    wget "https://$URL" --no-check-certificate --tries=3
     
     if ! $QUIET; then
         echo "==> Searching for internal domains..."
